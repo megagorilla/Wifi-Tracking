6957bb54d4f381a64d65165eb56f171aeed7fe0d
Christiaan Boerkamp <Christiaan Boerkamp>
2016-05-09 14:47:06 +0200

    nieuwe functionaliteit selecteren radii voor calculator

--- Server/server.py
+++ Server/server.py
@@ -12,7 +12,7 @@ class server:
 	
 	
 	
-'''
+'''	
 	def setLocation(self):
 		allids = self.Databaseserver.getIDs()
 		for id in allids:
@@ -26,13 +26,28 @@ class server:
 '''					
 
 	def setLocation(self):
-		Radii =  self.Databaseserver.getinfoforcalculatorquickversion(id)
-		if len(Radii) >= 2:
-			calcutedpoint = self.Calculator.calculatepoint(Radii)
-			if len(calculatepoint[0]) = 3: 
-				self.Databaseserver.setLocations(id,time.strftime('%Y-%m-%d %H:%M:%S'), calculatepoint[0][0], calculatepoint[0][1])
+		ALLRadii =  self.Databaseserver.getinfoforcalculatorquickversion(id)
+		lastforradiiid
+		counter = 0
+		splitpoints
+		#splitarray
+		for radii in ALLRadii:
+			if lastforradiiid != radii[0]:
+				lastforradiiid = radii[0]
+				radii.pop(0)
+				splitarray = radii[ splitpoints[-1]: counter + 1 ]
+				splitpoints.append(counter)				
+				if len(splitarray) >= 2:
+					calcutedpoint = self.Calculator.calculatepoint(Radii)
+					if len(calculatepoint[0]) = 3: 
+						self.Databaseserver.setLocations(id,time.strftime('%Y-%m-%d %H:%M:%S'), calculatepoint[0][0], calculatepoint[0][1])
+					else:
+						self.Databaseserver.setLocations(id,time.strftime('%Y-%m-%d %H:%M:%S'), calculatepoint[0][0], calculatepoint[0][1], calculatepoint[0][2])
 			else:
-				self.Databaseserver.setLocations(id,time.strftime('%Y-%m-%d %H:%M:%S'), calculatepoint[0][0], calculatepoint[0][1], calculatepoint[0][2])
+				lastforradiiid = radii[0]
+				radii.pop(0)
+			counter += 1
+			
 					
 	def startcalculator(self)
 		while True:
