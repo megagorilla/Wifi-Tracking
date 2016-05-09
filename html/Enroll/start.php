2a24b2392951a29262d5c16545a625a4b9106e25
megagorilla <sanderenbart@gmail.com>
2016-05-09 13:11:19 +0200

    fixed colour selection (AGAIN!!)

--- html/Enroll/start.php
+++ html/Enroll/start.php
@@ -1,14 +1,14 @@
 <?php
 
-	echo "<h1> Create A User</h1> ";
+	echo "<h1>Enroll for Wifi-Tracking</h1> ";
 	echo "<form action='create.php' method='post'>";
-	echo "Name<input type='text' name='inputName' value='' /> <br />";
-	echo "Macadress<input type='text' name='inputMacadress' value='' />";
-	echo "<br />";
+	echo "Name<br><input type='text' name='inputName' value='' /> <br />";
+	echo "Macadress<br><input type='text' name='inputMacadress' value='' />";
+	echo "<br /><br>";
 	echo "<input type='submit' name='submit' />";
 	echo "</form>";
 
-	$servername = 'localhost';
+	/*$servername = 'localhost';
 	$dbuser = 'root';
 	$password = 'Biertaart';
 	$dbname = 'tracking';
@@ -24,5 +24,5 @@
 		echo "<h3h>" . $person['Username'] . "</h3h>";
 		echo "<p>" . $person['MacHash'] . "</p>";
 
-	}
+	}*/
 ?>
