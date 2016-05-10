c61c8f01262c2f249ce067d9a91068f595a83f15
megagorilla <sanderenbart@gmail.com>
2016-05-10 13:04:38 +0200

    added mac adress check to website

--- html/Enroll/create.php
+++ html/Enroll/create.php
@@ -11,17 +11,16 @@
 	}
 	$Name = $_POST['inputName'];
 	$Macadress = $_POST['inputMacadress'];
-
-	if(!$_POST['submit'])	{
+	/*if(!$_POST['submit'])	{
 		echo "please fill in these forms so you can use indoor location tracking";
-	} else {
-		$query = "INSERT INTO Users (Username, MacHash) VALUES('" . $Name . "', '" . hash("sha512",$Macadress) . "')";
-		if (!mysqli_query ($conn,$query)){
-			echo("Error description: " . mysqli_error($conn));
-		}
-		else{
-			echo "user has been added";
-		}
+	} else {*/
+	$query = "INSERT INTO Users (Username, MacHash) VALUES('" . $Name . "', '" . hash("sha512",$Macadress) . "')";
+	if (!mysqli_query ($conn,$query)){
+		echo("Error description: " . mysqli_error($conn));
 	}
+	else{
+		echo "user has been added";
+	}
+	//}
 	mysqli_close($conn);
 ?>
